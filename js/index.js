9816a3bf0f05022708a14a3b9131dbd8ad09dc2f
Andrew Willoughby <30395608+willoughbylabs@users.noreply.github.com>
2019-11-17 16:32:51 -0600

    Style: add navbar color scroll efect

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 $(function () {
     $(document).scroll(function() {
-        var $nav = $("#main-nav");
+        var $nav = $("#nav-container");
+        var $link = $("a.nav-link");
+        var $brand = $("a.navbar-brand");
+        var $toggler = $(".navbar-toggler-icon");
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        $link.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        $brand.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        $toggler.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
