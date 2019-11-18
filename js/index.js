3b45770e15b411751b53d474b1a064f3395f680f
Andrew Willoughby <30395608+willoughbylabs@users.noreply.github.com>
2019-11-18 00:29:51 -0600

    Release: version 2.0 Bootstrap 4 build

--- js/index.js
+++ js/index.js
@@ -3,10 +3,12 @@ $(function () {
         var $nav = $("#nav-container");
         var $link = $("a.nav-link");
         var $brand = $("a.navbar-brand");
-        var $toggler = $(".navbar-toggler-icon");
+        var $hamburger = $(".hamburger");
+        var $toggler = $(".navbar-toggler");
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
         $link.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
         $brand.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        $hamburger.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
         $toggler.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
